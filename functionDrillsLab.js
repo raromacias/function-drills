93f522a64d465f20db192697fd543391cdbe2335
Raro Macias <ggmacias@gmail.com>
2022-08-22 19:24:12 -0400

    final code

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -287,8 +287,15 @@ let loser = 'Glimmer'
 */
 
 //CODE HERE
-
-
+function theEliminator(contestants, loser){
+  for(let i = 0; i < contestants.length; i++){
+    if(contestants[i] === loser){
+      contestants.splice(i, 1)
+    } 
+  }
+  return contestants
+}
+console.log(theEliminator(contestants, loser))
 ////////////////// PROBLEM 17 ////////////////////
 let sampleString = "Hi, my name is Kylo."
 /*
@@ -340,13 +347,25 @@ console.log(emailCheck("ggmaciasgmail.com"))
 
 
 ////////////////// PROBLEM 21 ////////////////////
-let sampleArray = [0,1,2,3,4,7,5,6,8,9]
+let sampleArray = [0,1,2,3,4,5,6,8,9]
 /*
   Write a function that takes in an array of numbers as an argument. In the body of the function, write logic to determine if the array is in ascending order. The function should return true, if it is sorted in ascending order, false if it is not. Create a variable, `arrayIsAscending` and set it equal to your function invoked. Use the sample array to test this function.
 */
 
 //CODE HERE
-
+function checkArrayAscending(arr){
+  let comparisonvalue = arr[0]
+  for (let i = 1; i < arr.length; i++){
+    if (arr[i] < comparisonvalue){
+      return false
+    } else {
+      comparisonvalue = arr[i]
+    }
+  }
+  return true
+}
+let arrayIsAscending = checkArrayAscending(sampleArray)
+console.log(arrayIsAscending)
 
 ////////////////// PROBLEM 22 ////////////////////
 
